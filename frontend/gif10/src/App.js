6ff9dce1cd872ced331621d12934989b22d334a8
Bredell_Evans <bredy452@gmail.com>
2021-04-19 19:47:26 -0400

    login/registration/logout components add. unpdated app.js

--- frontend/gif10/src/App.js
+++ frontend/gif10/src/App.js
@@ -1,8 +1,20 @@
   
 import React, { Component } from "react"; 
 import NewForm from './Components/NewForm'
+import Logout from '/Components/Logout'
+import Register from '/Components/.js'
+import Login from '/Components/.js'
 // import Edit from './Components/Edit'
 import ShowGifs from './Components/ShowGifs'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useHistory,
+  useLocation
+} from "react-router-dom"
 
 let baseUrl = ''
 
@@ -13,21 +25,46 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 
-class App extends Component {
+export default class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      user: [],
+      session: false,
       gifs:[]
     }
   }
 
-  getGifs = () => {
-    fetch(`${baseUrl}/gifs`).then(res => { 
-      return res.json()}).then(data => {
-        this.setState({
-          gifs: data,
-      })
+  checkLogin = () => {
+    console.log(this.state.session)
+  }
+
+
+  addUser = (newUser) => {
+    const copyUser = [...this.state.user]
+    copyUser.push(newUser)
+    this.setState({
+      user: copyUser,
+    })  
+  }
+
+  getUser = () => {
+    console.log(this.state.user)
+    this.checkLogin()
+    this.setState({
+      session: !this.state.session
     })
+  }
+
+  getGifs = () => {
+    fetch(`${baseUrl}/gifs`)
+    .then(res => { 
+      return res.json()})
+    .then(data => {
+        this.setState({
+          gifs: data
+        })
+      })
   }
 
   addGif = (newGif) => {
@@ -38,6 +75,26 @@ class App extends Component {
       gifs: copyGifs,
       name: ''
     }) 
+  }
+
+  addSession = (newSession) => {
+    console.log("addsess")
+    const copySession = [...this.state.user]
+    copySession.push(newSession)
+    this.setState({
+      user: copySession,
+      session: true
+    })
+  }
+
+  deleteSession = (deletedSession) => {
+    const findIndex = this.state.user.findIndex(user => deletedSession._id === user._id)
+        const copySession = [...this.state.user]
+        copySession.splice(findIndex, 1)
+        this.setState({
+          user: copySession,
+          session: false
+        })
   }
 
   
@@ -51,16 +108,33 @@ class App extends Component {
   render() {
     console.log(this.state)
     console.log(baseUrl)
-    return (
-     <div className='container'>
-       <h1>The Amazing Giph App!</h1>
-       <NewForm baseUrl={baseUrl} addGifs={this.addGif}/>
-       <ShowGifs newGif={this.state.gifs} getGifs={this.getGif} baseUrl={baseUrl}/>
 
-       {/*<Delete newGif={this.state.gifs} baseUrl={baseUrl} getGifs={this.getGifs} />
+    return (
+  
+ //      <div  >
+ //        {(() => {
+ //          if (user) {
+ //            return <Logout getUser={this.getUser} baseUrl={baseUrl} deleteSession={this.deleteSession} user={this.user} />
+ //          } else {
+ //              return <Login baseUrl={baseUrl} addSessions={this.addSession} />   
+ //          }
+ //        })
+ //        ()}
+ //    {/* <Logout getUser={this.getUser} baseUrl={baseUrl} user={this.user[0]} /> 
+ // <Login baseUrl={baseUrl} addSessions={this.addSession} />  */}
+ //      <Register baseUrl={baseUrl} addUser={this.addUser}/>
+
+ //      </div>
+
+      <div className='container'>
+           <h1>The Amazing Giph App!</h1>
+          <NewForm baseUrl={baseUrl} addGifs={this.addGif}/>
+          <ShowGifs newGif={this.state.gifs} getGifs={this.getGif} baseUrl={baseUrl}/>
+
+          {/*<Delete newGif={this.state.gifs} baseUrl={baseUrl} getGifs={this.getGifs} />
        <Edit newGif={this.state.gifs} baseUrl={baseUrl} getGifs={this.getGifs}/> */}
                  
-       {/*<table>
+           {/*<table>
          <tbody>
            {this.state.gifs.map(gif => {
              return (
@@ -77,10 +151,27 @@ class App extends Component {
              )
            })}
          </tbody>
-       </table>*/}
-     </div>
-    );
+           </table>*/}
+      </div>
+    )
   }
 }
 
-export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
